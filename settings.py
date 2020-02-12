714e9f07eed1e07c6a49014ee014561158485fcd
Naik <divya.naik@leybold.com>
2020-02-12 09:31:20 +0100

    First commit Message

---
+++ settings.py
@@ -0,0 +1,16 @@
+# c8y
+C8Y_USER = 'divya.naik@leybold.com'
+C8Y_PASSWORD = 'Gerdia3#' 
+
+# data_api
+API_USER = 'dvnaik'
+API_PASSWORD = 'itsme3#3'
+#API_USER = 'leybold-gmbh/divya-naik'
+#API_PASSWORD = 'f0PVWCc6V1fUOYwEOq9pp8ACLbPfbWcCvdBGbeSp6R9q6weifukcXBnSzkC2'
+#API_URL = 'https://web.postman.co/settings/me/api-keys'
+#SECRET_KEY = 'PMAK-5e30631ac62f5500309d00fa-5009bead9a1fb36ea20565da3d92dddef0'
+#DEBUG = True
+#MONGODB_HOST = '172.31.48.103' # AWS mongodb VCN ip
+# MONGODB_HOST = 'localhost' # local
+# MONGODB_HOST = '18.185.244.89' # AWS mongodb public ip
+#MONGODB_DB = 'data_api'
